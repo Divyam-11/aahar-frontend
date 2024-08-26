72bd9a2ae7045ed7e5e84e9858daae1fcf0e235c
Innisfree-11 <rogersdivyam@gmail.com>
2024-08-26 19:15:37 +0530

    asd,fjksa;lfa

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,11 +2,13 @@ import './header.css'
 import logo from "../assets/images/logo.svg"
 import lang_logo from"../assets/images/language_icon.svg"
 import down_arrow_icon from "../assets/images/down-arrow-icon.svg"
+import {Link} from "react-router-dom";
 function Header(){
 
     return(<div className="main">
 
-        <img src={logo} className="logo" alt="logo"/>
+        <Link to={"/"}><img src={logo} className="logo" alt="logo"/></Link>
+
         <div className="langs">
             <img src={lang_logo} alt="language" />
             <p>Language</p>

f6a2b17faaf4cae6017ae76291116682c94237a1
Innisfree-11 <rogersdivyam@gmail.com>
2024-08-29 18:50:10 +0530

    asd,fjksa;lfa

--- src/Components/youtubeEmbed'.jsx
+++ src/Components/youtubeEmbed'.jsx
@@ -4,7 +4,7 @@ const YouTubeEmbed = () => {
     return (
         <div className="yt_embed">
             <p>Farming Tips</p>
-        <iframe width="245" height="245" src="https://www.youtube.com/embed/QdBZY2fkU-0?si=KypgQfPWljvLBCRn"
+        <iframe width="245" height="245" src="https://www.youtube.com/embed/3JigXb9KXqI?si=TuUuhkBVy1cPgYHd"
                 title="YouTube video player" frameBorder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                 referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
